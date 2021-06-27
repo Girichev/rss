ef6cbd471ffb2ce4d664723edd30e75a7b41e0c7
Hienadz <girichev@gmail.com>
2021-06-27 21:55:01 +0200

    Channel DAO small fixes

--- app/src/main/java/com/sample/rss/room/dao/RssChannelDao.kt
+++ app/src/main/java/com/sample/rss/room/dao/RssChannelDao.kt
@@ -3,38 +3,26 @@ package com.sample.rss.room.dao
 import androidx.annotation.WorkerThread
 import androidx.room.Dao
 import androidx.room.Query
-import androidx.room.rxjava3.EmptyResultSetException
 import com.sample.rss.room.BaseDao
 import com.sample.rss.room.entity.RssChannelEntity
-import com.sample.rss.room.entity.RssItemEntity
-import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.disposables.Disposable
-import io.reactivex.rxjava3.kotlin.subscribeBy
-import io.reactivex.rxjava3.schedulers.Schedulers
 import timber.log.Timber
 
 @Dao
 interface RssChannelDao : BaseDao<RssChannelEntity> {
 
-    @Query("SELECT * FROM rss_item WHERE title = :title LIMIT 1")
-    fun getOneByTitle(title: String): Single<RssItemEntity>
+    @Query("SELECT * FROM rss_channel WHERE title = :title LIMIT 1")
+    fun getOneByTitle(title: String): RssChannelEntity?
 
     @WorkerThread
-    fun insertOrUpdate(entity: RssChannelEntity): Disposable {
-        return getOneByTitle(entity.link).subscribeOn(Schedulers.io())
-            .subscribeBy({
-                it.printStackTrace()
-                if(it is EmptyResultSetException) {
-                    insert(entity)
-                }
-            }, {
-                if(it.equals(entity).not()) {
-                    // Need update
-                    update(entity)
-                } else {
-                    // Content the same
-                    Timber.d("Skip update content the same")
-                }
-            })
+    fun insertOrUpdate(entity: RssChannelEntity) {
+        getOneByTitle(entity.link)?.let {
+            if(it.equals(entity).not()) {
+                update(entity)
+            } else {
+                Timber.d("Skip update content the same")
+            }
+        } ?: kotlin.run {
+            insert(entity)
+        }
     }
 }
